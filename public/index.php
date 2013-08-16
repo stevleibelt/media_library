163f76ec6cc83c4f46ca2fa240c15da9aee580ef
stevleibelt <stev@leibelt.de>
2013-08-16 15:23:30 +0200

    Added index file

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * based on: https://github.com/khepin/tsusbos/blob/master/web/index.php
+ *
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-16
+ */
+
+$application = require dirname(__FILE__) . '/../source/application.php';
+
+$application->run();
