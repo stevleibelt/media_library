c7f704c966ae338534a40695f87ac6d1b400c655
stevleibelt <stev@leibelt.de>
2013-08-16 15:30:36 +0200

    Initial application file started

---
+++ source/Net/Bazzline/MediaLibrary/application.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-16
+ */
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+//bootstrap
+
+$application = new Silex\Application();
+$application['key'] = 'media_library_2013-08-16';
+
