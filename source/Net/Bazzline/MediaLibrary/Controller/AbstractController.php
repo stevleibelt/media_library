ce5028593498918f7ce49bc6cf8b67270f74f8ad
stevleibelt <stev@leibelt.de>
2013-08-18 01:56:56 +0200

    Added constructor

--- source/Net/Bazzline/MediaLibrary/Controller/AbstractController.php
+++ source/Net/Bazzline/MediaLibrary/Controller/AbstractController.php
@@ -8,6 +8,9 @@ namespace Net\Bazzline\MediaLibrary\Controller;
 
 use Silex\Controller;
 
+use Net\Bazzline\MediaLibrary\Application\Application;
+use Net\Bazzline\MediaLibrary\Model\IO\Request;
+
 /**
  * Class AbstractController
  *
@@ -17,5 +20,29 @@ use Silex\Controller;
  */
 class AbstractController extends Controller
 {
+    /**
+     * @var \Net\Bazzline\MediaLibrary\Application\Application
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-18
+     */
+    protected $application;
 
+    /**
+     * @var \Net\Bazzline\MediaLibrary\Model\IO\Request
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-18
+     */
+    protected $request;
+
+    /**
+     * @param Application $application
+     * @param Request $request
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-18
+     */
+    public function __construct(Application $application, Request $request)
+    {
+        $this->application = $application;
+        $this->request = $request;
+    }
 }
